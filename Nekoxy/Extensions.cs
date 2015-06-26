47269ba04ed8970c1953220d750b5e88a26e1e9e
veigr <veigr@cat-ears.net>
2015-06-26 16:03:14 +0900

    ・Transfer-Encoding: chunked なレスポンスデータをそのままクライアントに送信するよう変更 ・NuGetインストール時にLICENSEへの同意を求めるよう変更

--- Nekoxy/Extensions.cs
+++ Nekoxy/Extensions.cs
@@ -40,8 +40,67 @@ namespace Nekoxy
             return addresses.Any(IPAddress.IsLoopback);
         }
 
+        /// <summary>
+        /// ChunkedDataを送信。
+        /// destとhandler両方指定できないと困るので作成。
+        /// オリジナルのものはinternalでアクセス出来ない。
+        /// </summary>
+        /// <param name="src">送信元</param>
+        /// <param name="dest">送信先</param>
+        /// <param name="handler">パケットハンドラ</param>
+        public static void TunnelChunkedDataTo(
+            this HttpSocket src,
+            HttpSocket dest,
+            HttpSocket.MessagePacketHandler handler)
+        {
+            while (true)
+            {
+                var chunkHeader = src.ReadAsciiLine();
+                if (chunkHeader.Length == 0)
+                    throw new HttpProtocolBroken("Expected chunk header missing");
+
+                var hSize = new string(chunkHeader.TakeWhile(x => !chunkSizeEnd.Contains(x)).ToArray());
+                uint size;
+                try
+                {
+                    size = Convert.ToUInt32(hSize, 16);
+                }
+                catch
+                {
+                    var s = chunkHeader.Length > 20
+                        ? (chunkHeader.Substring(0, 17) + "...")
+                        : chunkHeader;
+                    throw new HttpProtocolBroken("Could not parse chunk size in: " + s);
+                }
+
+                if (dest != null) dest.WriteAsciiLine(chunkHeader);
+
+                if (size == 0) break;
+
+                src.TunnelDataTo(handler, size);
+
+                var newLine = src.ReadAsciiLine();
+
+                if (dest != null) dest.WriteAsciiLine(newLine);
+            }
+
+            string line;
+            do
+            {
+                line = src.ReadAsciiLine();
+                if (dest != null) dest.WriteAsciiLine(line);
+            } while (line.Length != 0);
+        }
+
         private static readonly Encoding defaultEncoding = Encoding.ASCII;
         private static readonly Regex charsetRegex = new Regex("charset=([\\w-]*)", RegexOptions.Compiled);
         private static readonly Regex mimeTypeRegex = new Regex("^([^;]+)", RegexOptions.Compiled);
+        private static readonly char[] chunkSizeEnd = { ' ', ';' };
+
+    }
+
+    public class HttpProtocolBroken : Exception
+    {
+        internal HttpProtocolBroken(string msg) : base(msg) { }
     }
 }
--- NekoxyExample/Program.cs
+++ NekoxyExample/Program.cs
@@ -12,9 +12,9 @@ namespace NekoxyExample
         private static void Main(string[] args)
         {
             HttpProxy.Shutdown();
-            HttpProxy.Startup(37564);
-            //HttpProxy.UpstreamProxyHost = "127.0.0.1";
-            //HttpProxy.UpstreamProxyPort = 8888;
+            HttpProxy.UpstreamProxyHost = null;
+            HttpProxy.UpstreamProxyPort = 1;
+            HttpProxy.Startup(12345);
             HttpProxy.AfterSessionComplete += s => Task.Run(() => Console.WriteLine(s.Request.RequestLine));
             HttpProxy.AfterSessionComplete += s => Task.Run(() => Console.WriteLine(s.Response.StatusLine));
             HttpProxy.AfterSessionComplete += s => Task.Run(() => Debug.WriteLine(s.Request.RequestLine));
